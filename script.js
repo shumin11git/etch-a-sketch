4aa1a8a7712ec94ac1e65599888823736314f60b
shumin11git <shimshonwi@gmail.com>
2023-11-09 19:58:16 +0200

    Add switch colors button

--- script.js
+++ script.js
@@ -1,53 +1,51 @@
 //grid size in pixels
-
 const gridSidePX = 500;
+//maximum cells on each side
 const maxGridSize = 100;
 
-//--- initializing grid and squares --->
+//--- initializing page content --->
 
+//page elements
 const gridSizeButton = document.querySelector('#grid-size-button');
 const gridSizeInput = document.querySelector('#grid-size-input');
 const clearButton = document.querySelector('#clear-button');
+const switchColorsButton = document.querySelector('#switch-colors-button');
+
+//grid and squares
 const grid = document.querySelector('#grid');
 const singleSquare = document.createElement('div');
-
 createGrid(gridSizeInput.value);
 
-//--- event listeners for buttons --->
-
-gridSizeButton.addEventListener('click', recreateGrid);
-
+//--- event listeners --->
+//apply size input with button
+gridSizeButton.addEventListener('click', e => {
+    recreateGrid(true)
+});
+//apply size input with keyboard
 gridSizeInput.addEventListener('keyup', e => {
     if (e.key === 'Enter') {
-        recreateGrid();
+        recreateGrid(true);
     }
 })
-
-clearButton.addEventListener('click', recreateGrid);
+//clear board
+clearButton.addEventListener('click', e => {
+    let squaresClass = document.querySelectorAll('.square');
+    squaresClass.forEach(node => addClass(node, 'initial', 'hover1'));
+});
+//switch colors
+let hoverClasses = ['hover1', 'hover2', 'hover3', 'hover4'];
+//let randomHoverClass = hoverClasses[Math.floor(Math.random() * hoverClasses.length)];
+let hoverClassIndex = 0;
+switchColorsButton.addEventListener('click', e => {
+    if (hoverClassIndex >= hoverClasses.length - 1) hoverClassIndex = 0;
+    else hoverClassIndex++;
+    console.log(hoverClassIndex);
+    recreateGrid(false, hoverClasses[hoverClassIndex]);
+})
 
 //--- functions --->
 
-function fillGridWithNSquares(n) {
-    for (let i = 1; i <= n; i++) {
-        const singleSquareClone = singleSquare.cloneNode(true);
-        grid.appendChild(singleSquareClone);
-    };
-}
-
-function addEventAndClass(node, event, classToAdd, classToRemove) {
-    node.addEventListener(`${event}`, e => {
-        node.classList.add(`${classToAdd}`);
-        node.classList.remove(`${classToRemove}`);
-    });
-}
-
-function addEventAndClassForEach(nodeList, event, classToAdd, classToRemove) {
-    nodeList.forEach(node =>
-        addEventAndClass(node, event, classToAdd, classToRemove)
-    );
-}
-
-function createGrid(size) {
+function createGrid(size, hoverClass='hover1') {
     if (size > maxGridSize || size < 1) return;
     //setting up square
     let gridSideCells = size;
@@ -60,16 +58,45 @@ function createGrid(size) {
     grid.style.width = `${gridSideCells * squareSide}px`;
     grid.style.height = `${gridSideCells * squareSide}px`;
     fillGridWithNSquares(gridSize);
-    addHoverToSquares();
+    addHoverToSquares(hoverClass);
 }
 
-function recreateGrid() {
-    if (gridSizeInput.value > maxGridSize || gridSizeInput.value < 1) return;
+function fillGridWithNSquares(n) {
+    for (let i = 1; i <= n; i++) {
+        const singleSquareClone = singleSquare.cloneNode(true);
+        grid.appendChild(singleSquareClone);
+    };
+}
+
+function recreateGrid(checkInput, hoverClass) {
+    if (checkInput) {
+        if (gridSizeInput.value == grid.childNodes.length ** (1/2)) return;
+    }
+    if (gridSizeInput.value > maxGridSize || 
+        gridSizeInput.value < 1)
+        return;
     grid.innerHTML = '';
-    createGrid(gridSizeInput.value);
+    createGrid(gridSizeInput.value, hoverClass);
 }
 
-function addHoverToSquares() {
+function addClass(node, classToAdd, classToRemove) {
+    node.classList.add(`${classToAdd}`);
+    if (node.classList.contains(classToRemove)) {
+        node.classList.remove(`${classToRemove}`);
+    }
+}
+
+function addEventAndClass(node, event, classToAdd, classToRemove) {
+    node.addEventListener(`${event}`, e => addClass(node, classToAdd, classToRemove));
+}
+
+function addEventAndClassForEach(nodeList, event, classToAdd, classToRemove) {
+    nodeList.forEach(node =>
+        addEventAndClass(node, event, classToAdd, classToRemove)
+    );
+}
+
+function addHoverToSquares(hoverClass) {
     let squaresClass = document.querySelectorAll('.square');
-    addEventAndClassForEach(squaresClass, 'mouseover', 'hover', 'initial');
+    addEventAndClassForEach(squaresClass, 'mouseover', hoverClass, 'initial');
 }
