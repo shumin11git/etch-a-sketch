f269cde1391985e7d017c7f5f361b4bde5a1baba
shumin11git <shimshonwi@gmail.com>
2023-11-09 10:51:20 +0200

    Add color change on hover to grid squares

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-//grid size
-let gridSidePX = 500;
-let gridSideCells = 16;
+//grid size variables
+const gridSidePX = 500;
+const gridSideCells = 16;
 const squareSide = gridSidePX/gridSideCells;
 
+
+//setting up single square
+const singleSquare = document.createElement('div');
+singleSquare.style.width = `${squareSide}px`;
+singleSquare.style.height = `${squareSide}px`;
+singleSquare.classList.add('square', 'initial');
 //setting up grid
 const grid = document.querySelector('#grid');
 const gridSize = gridSideCells * gridSideCells;
@@ -11,20 +17,26 @@ grid.style.height = `${gridSideCells * squareSide}px`;
 fillGridWithNSquares(gridSize);
 
 
-
-
+//event listeners for square
+const squaresClass = document.querySelectorAll('.square');
+squaresClass.forEach(item => addHover(item));
+//squareClass.addEventListener('click', e => squareClass.style['background-color'] = 'white');
+//squareClass.
 
 
 
 
 //functions
 function fillGridWithNSquares(n) {
-    const singleSquare = document.createElement('div');
-    singleSquare.style.width = `${squareSide}px`;
-    singleSquare.style.height = `${squareSide}px`;
-    singleSquare.classList.add('square', 'initial');
     for (let i = 1; i <= n; i++) {
         const singleSquareClone = singleSquare.cloneNode(true);
         grid.appendChild(singleSquareClone);
     }
+}
+
+function addHover(item) {
+    item.addEventListener('mouseover', e => {
+        item.classList.remove('initial');
+        item.classList.add('hover');
+    });
 }
